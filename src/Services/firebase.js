22d295cf9189e2c55f272637c0a4be2ad37ec6f9
roverflow <roverflow5@gmail.com>
2021-05-16 22:12:56 +0530

    Update FireBase Env variables
    Changes in firebase variables

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC-YzzdF8XkWTSsfb_3DE_henGZvn9P6zo",
-    authDomain: "lbtc-website.firebaseapp.com",
-    databaseURL: "https://lbtc-website.firebaseio.com",
-    projectId: "lbtc-website",
-    storageBucket: "lbtc-website.appspot.com",
-    messagingSenderId: "808094427144",
-    appId: "1:808094427144:web:8b506e3ce61aeb794589bd",
-    measurementId: "G-E238X4JKE4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
